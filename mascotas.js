bf77ceacc1c69d244e34b267a58275497a5c1e36
Daniela Acero <danielalizethacero@gmail.com>
2023-05-02 20:44:55 -0500

    configuración con objetos

--- mascotas.js
+++ mascotas.js
@@ -1,16 +1,76 @@
 let tablaMascotas = document.getElementById("tabla-mascotas");
 let cuerpoTabla = tablaMascotas.querySelector("tbody");
 
-//Arreglos
-let nombreMascota = ["Luna","Orion"]
-let nombreDueño = ["Ana","Jorge"]
-let cedulaDueño = ["1234567","73412938"]
-let edadMascota = ["5 años","2 meses"]
-let telefonoDueño = ["3108888423","1234567890"]
-let especialidad = ["Veterinaria Conductual","Medicina Interna"]
-let medico = ["Jorge","Daniela"]
+// //Arreglos
+// let nombreMascota = ["Luna","Orion"]
+// let nombreDueño = ["Ana","Jorge"]
+// let cedulaDueño = ["1234567","73412938"]
+// let edadMascota = ["5 años","2 meses"]
+// let telefonoDueño = ["3108888423","1234567890"]
+// let especialidad = ["Veterinaria Conductual","Medicina Interna"]
+// let medico = ["Jorge","Daniela"]
 
-for (let i=0; i < nombreMascota.length; i++){
+// for (let i=0; i < nombreMascota.length; i++){
+//     let fila = cuerpoTabla.insertRow();
+//     let celdaNombreMascota = fila.insertCell();
+//     let celdaNombreDueño = fila.insertCell();
+//     let celdaCedulaDueño = fila.insertCell();
+//     let celdaEdadMascota = fila.insertCell();
+//     let celdaTelefonoDueño = fila.insertCell();
+//     let celdaEspecialidad = fila.insertCell();
+//     let celdaMedico = fila.insertCell();
+
+//     celdaNombreMascota.textContent = nombreMascota[i];
+//     celdaNombreDueño.textContent = nombreDueño[i];
+//     celdaCedulaDueño.textContent = cedulaDueño[i];
+//     celdaEdadMascota.textContent = edadMascota[i];
+//     celdaTelefonoDueño.textContent = telefonoDueño[i];
+//     celdaEspecialidad.textContent = especialidad[i];
+//     celdaMedico.textContent = medico[i];
+// }
+
+//objetos
+let mascotas = [
+    {
+        nombreMascota:"Luna",
+        nombreDueño: "Ana",
+        cedulaDueño: "1234567",
+        edadMascota: "5 años",
+        telefonoDueño: "3108888423",
+        especialidad: "Veterinaria Conductual",
+        medico:"Jorge"
+    },
+    {
+        nombreMascota:"Orion",
+        nombreDueño: "Jorge",
+        cedulaDueño: "73412938",
+        edadMascota: "2 meses",
+        telefonoDueño: "1234567890",
+        especialidad: "Medicina Interna",
+        medico:"Daniela"
+    },
+    {
+        nombreMascota:"Maily",
+        nombreDueño: "Kevin",
+        cedulaDueño: "59238237",
+        edadMascota: "2 años",
+        telefonoDueño: "31029384759",
+        especialidad: "Dermatología veterinaria",
+        medico: "Andres"
+    },
+    {
+        nombreMascota:"Mistyc",
+        nombreDueño: "Nicolas",
+        cedulaDueño: "97623191",
+        edadMascota: "8 años",
+        telefonoDueño: "30076391823",
+        especialidad: "Farmacología veterinaria",
+        medico:"Jeisson"
+    }
+]
+
+for (let i=0; i < mascotas.length; i++){
+    let mascota = mascotas[i]
     let fila = cuerpoTabla.insertRow();
     let celdaNombreMascota = fila.insertCell();
     let celdaNombreDueño = fila.insertCell();
@@ -20,12 +80,11 @@ for (let i=0; i < nombreMascota.length; i++){
     let celdaEspecialidad = fila.insertCell();
     let celdaMedico = fila.insertCell();
 
-    celdaNombreMascota.textContent = nombreMascota[i];
-    celdaNombreDueño.textContent = nombreDueño[i];
-    celdaCedulaDueño.textContent = cedulaDueño[i];
-    celdaEdadMascota.textContent = edadMascota[i];
-    celdaTelefonoDueño.textContent = telefonoDueño[i];
-    celdaEspecialidad.textContent = especialidad[i];
-    celdaMedico.textContent = medico[i];
-
+    celdaNombreMascota.textContent = mascota.nombreMascota;
+    celdaNombreDueño.textContent = mascota.nombreDueño;
+    celdaCedulaDueño.textContent = mascota.cedulaDueño;
+    celdaEdadMascota.textContent = mascota.edadMascota;
+    celdaTelefonoDueño.textContent = mascota.telefonoDueño;
+    celdaEspecialidad.textContent = mascota.especialidad;
+    celdaMedico.textContent = mascota.medico;
 }
--- medicos.js
+++ medicos.js
@@ -1,15 +1,58 @@
 let tablaMedicos = document.getElementById("tabla-medicos");
 let cuerpoTabla = tablaMedicos.querySelector("tbody");
 
-let nombre = ["a"]
-let apellido = ["b"]
-let cedula = ["c"]
-let especialidad = ["d"]
-let consultorio = ["e"]
-let correo = ["f"]
-let paciente = ["g"]
+// // Arreglos
+// let nombre = ["a"]
+// let apellido = ["b"]
+// let cedula = ["c"]
+// let especialidad = ["d"]
+// let consultorio = ["e"]
+// let correo = ["f"]
+// let paciente = ["g"]
 
-for(let i=0; i<nombre.length; i++){
+// for(let i=0; i<nombre.length; i++){
+//     let fila = cuerpoTabla.insertRow();
+//     let celdaNombre = fila.insertCell();
+//     let celdaApellido = fila.insertCell();
+//     let celdaCedula = fila.insertCell();
+//     let celdaEspecialidad = fila.insertCell();
+//     let celdaConsultorio = fila.insertCell();
+//     let celdaCorreo = fila.insertCell();
+//     let celdaPaciente = fila.insertCell();
+
+//     celdaNombre.textContent = nombre[i];
+//     celdaApellido.textContent = apellido[i];
+//     celdaCedula.textContent = cedula[i];
+//     celdaEspecialidad.textContent = especialidad[i];
+//     celdaConsultorio.textContent = consultorio[i];
+//     celdaCorreo.textContent = correo[i];
+//     celdaPaciente.textContent = paciente[i];
+// }
+
+//Objetos
+let medicos = [
+    {
+        nombre: "a",
+        apellido: "b",
+        cedula: "c",
+        especialidad: "d",
+        consultorio: "e",
+        correo: "f",
+        paciente: "g"
+    },
+    {
+        nombre: "h",
+        apellido: "i",
+        cedula: "j",
+        especialidad: "k",
+        consultorio: "l",
+        correo: "m",
+        paciente: "n"
+    }
+]
+
+for(let i=0;i<medicos.length;i++){
+    let medico = medicos[i]
     let fila = cuerpoTabla.insertRow();
     let celdaNombre = fila.insertCell();
     let celdaApellido = fila.insertCell();
@@ -19,12 +62,11 @@ for(let i=0; i<nombre.length; i++){
     let celdaCorreo = fila.insertCell();
     let celdaPaciente = fila.insertCell();
 
-    celdaNombre.textContent = nombre[i];
-    celdaApellido.textContent = apellido[i];
-    celdaCedula.textContent = cedula[i];
-    celdaEspecialidad.textContent = especialidad[i];
-    celdaConsultorio.textContent = consultorio[i];
-    celdaCorreo.textContent = correo[i];
-    celdaPaciente.textContent = paciente[i];
-
+    celdaNombre.textContent = medico.nombre;
+    celdaApellido.textContent = medico.apellido;
+    celdaCedula.textContent = medico.cedula;
+    celdaEspecialidad.textContent = medico.especialidad;
+    celdaConsultorio.textContent = medico.consultorio;
+    celdaCorreo.textContent = medico.correo;
+    celdaPaciente.textContent = medico.paciente;
 }
